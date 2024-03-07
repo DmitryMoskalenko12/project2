fb28709ad76c2d82d3a537d0410f061f44624c5e
Dima Moskalenko <dmtrmsklnk@gmail.com>
2024-03-07 16:33:16 +0200

    add: Header code

--- src/hooks/useResizeX.jsx
+++ src/hooks/useResizeX.jsx
@@ -1,1 +1,18 @@
-export const useResizeX = () => {};
+import { useState, useEffect } from "react";
+export const useResizeX = (targetResize = 768) => {
+   const [resized, setResized] = useState(false);
+
+   useEffect(() => {
+    const handleResize = () => {
+        if (window.innerWidth > targetResize) {
+            setResized(true)
+        } else {
+            setResized(false)
+        }
+    }
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+   },[targetResize])
+   return resized;
+};
